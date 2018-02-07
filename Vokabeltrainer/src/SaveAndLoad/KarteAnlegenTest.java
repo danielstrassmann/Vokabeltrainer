187e8e89994623e610679c9df043bace51682626
strassmann <dstrassmann91@hotmail.com>
2018-02-07 15:53:11 +0100

    1

--- Vokabeltrainer/src/SaveAndLoad/KarteAnlegenTest.java
+++ Vokabeltrainer/src/SaveAndLoad/KarteAnlegenTest.java
@@ -1,8 +1,6 @@
 package SaveAndLoad;
 
 import java.io.File;
-import java.util.logging.ConsoleHandler;
-
 import Model.Karte;
 
 public class KarteAnlegenTest {
@@ -10,18 +8,6 @@ public class KarteAnlegenTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-		/*
-		 * try { Karte karte = new Karte ("hallo", "hello"); File xmlfile = new
-		 * File("karte.xml"); JAXBContext jaxbContext =
-		 * JAXBContext.newInstance(Karte.class); Marshaller jaxbMarshaller =
-		 * jaxbContext.createMarshaller();
-		 * 
-		 * jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		 * jaxbMarshaller.marshal(karte, xmlfile);
-		 * 
-		 * 
-		 * } catch(JAXBException ex) { System.out.println(ex); }
-		 */
 
 		AbspeichernLaden saveHandler = new AbspeichernLaden();
 
