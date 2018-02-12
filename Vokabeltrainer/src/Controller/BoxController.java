f18e49b63c5cce1094c5ae09181906a3a800f82a
brunnert <brunnert@brunnert-zb15.int.abacus.ch>
2018-02-12 22:01:01 +0100

    test

--- Vokabeltrainer/src/Controller/BoxController.java
+++ Vokabeltrainer/src/Controller/BoxController.java
@@ -2,9 +2,5 @@ package Controller;
 
 public class BoxController {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
--- Vokabeltrainer/src/Controller/KarteController.java
+++ Vokabeltrainer/src/Controller/KarteController.java
@@ -2,5 +2,5 @@ package Controller;
 
 public class KarteController {
 
-	private String text;
+
 }
