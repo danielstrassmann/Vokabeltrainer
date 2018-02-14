1427c0d7ff4bee2fd96bcb800724d987e487b82c
Atrais <marius.braendle@gmx.ch>
2018-02-14 20:44:48 +0100

    test

--- Vokabeltrainer/src/Viewer/Hauptmenu.java
+++ Vokabeltrainer/src/Viewer/Hauptmenu.java
@@ -81,7 +81,7 @@ public class Hauptmenu {
         		"Baden-Württemberg", "Bayern",
                 "Berlin", "Brandenburg", "Bremen",
                 "Hamburg", "Hessen", "Mecklenburg-Vorpommern",
-                "Niedersachsen", "Nordrhein-Westfalen", "Rheinland-Pfalz",
+                "Niedersachsen", "Nordrhein-Wesdtfalen", "Rheinland-Pfalz",
                 "Saarland", "Sachsen", "Sachsen-Anhalt",
                 "Schleswig-Holstein", "Thürindsfgeeneee"
                 };		
