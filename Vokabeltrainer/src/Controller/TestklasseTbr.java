8fa533a9fc13f8afc063e8898cb9691f396f74b6
tomygun66 <t.brunner92@hotmail.com>
2018-02-15 18:26:01 +0100

    Testklasse erweitert für Karten

--- Vokabeltrainer/src/Controller/TestklasseTbr.java
+++ Vokabeltrainer/src/Controller/TestklasseTbr.java
@@ -2,6 +2,7 @@ package Controller;
 
 import java.util.ArrayList;
 
+import Model.Karte;
 import Model.Kartei;
 import Model.User;
 
@@ -18,15 +19,52 @@ public class TestklasseTbr {
 		test.karteiErstellen("Deutsch", "Englisch", u);
 
 		// Get ArrayList vom zuvor instanzierten User
-		ArrayList<Kartei> liste;
-		liste = u.getUserKarteien();
+		ArrayList<Kartei> karteiliste;
+		karteiliste = u.getUserKarteien();
 
 		// Zuvor erstellte Kartei aus ArrayList auslesen
 		Kartei kk = new Kartei();
-		kk = liste.get(0);
+		kk = karteiliste.get(0);
 
-		// Attribute vom Objekt ausgeben um zu prüfen, ob alles klappt
+		
+		
+		// TEST FÜR KARTEI: Attribute vom Objekt ausgeben um zu prüfen, ob alles klappt
+		System.out.println("TEST für Kartei:");
+		System.out.println("-----------------");
 		System.out.println("Die Kartei " + kk.getFrage() + "-" + kk.getAntwort() + " besitzt die Id " + kk.getId());
+		System.out.println("");
+		System.out.println("");
+		
+
+		
+		
+				
+		// Karten in erstellte Kartei ablegen
+		test.karteInKarteiAblegen("Haus", "House", kk);
+		test.karteInKarteiAblegen("Wasser", "Water", kk);
+		test.karteInKarteiAblegen("Feuer", "Fire", kk);
+		
+		
+		// Get ArrayList von der Klasse Kartei
+		ArrayList<Karte> kartenliste;
+		kartenliste = kk.getSammlung();
+
+		
+		// Karten aus ArrayList in Klasse Kartei auslesen
+		Karte k1 = kartenliste.get(0);
+		Karte k2 = kartenliste.get(1); 
+		Karte k3 = kartenliste.get(2);
+		
+		
+		
+		// TEST FÜR KARTEN: Attribute von Objekten ausgeben und prüfen, ob alles klappt
+		System.out.println("TEST für Karten:");
+		System.out.println("-----------------");
+		System.out.println("Die Karte mit der Id " + k1.getId() + " hat die Antwort [" + k1.getFrage() + "] und die Antwort [" + k1.getAntwort() + "]");
+		System.out.println("Die Karte mit der Id " + k2.getId() + " hat die Antwort [" + k2.getFrage() + "] und die Antwort [" + k2.getAntwort() + "]");
+		System.out.println("Die Karte mit der Id " + k3.getId() + " hat die Antwort [" + k3.getFrage() + "] und die Antwort [" + k3.getAntwort() + "]");
+		
+		
 
 	}
 
