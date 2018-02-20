c8eecfea5366b05ff90539b51edf3e1e1f911f6d
Ducij <ducij.dt@gmail.com>
2018-02-20 21:28:11 +0100

    exitfenster

--- Vokabeltrainer/src/Viewer/Anmeldefenster.java
+++ Vokabeltrainer/src/Viewer/Anmeldefenster.java
@@ -1,79 +1,93 @@
 package Viewer;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
+import javax.swing.JPasswordField;
 
 public class Anmeldefenster{
 	
 	//Frame
 	private JFrame loginfenster;
+	private JFrame frmLoginSystem;
 	//Labels
 	private JLabel usernameid;
 	private JLabel userpassword;
+	private JLabel frameTitle;
 	//Buttons 
 	private JButton anmeldeButton;
 	private JButton registrieren;
 	private JButton exitButton;
 	//TextFelder
 	private JTextField loginUsername;
-	private JTextField loginPassword;
+	private JPasswordField loginPassword;
 	//JPanels
 	private JPanel userpw;
-	private JPanel userpwEingabe;
+	private JPanel titelframe;
 	private JPanel buttonBereich;
-	                         
-	private SpringLayout layout;
-	
+	private JPanel loginBereich;
+ 		
 public Anmeldefenster() 
 {
-    SpringLayout layout = new SpringLayout();
 	//GUI-Elements
-	this.loginfenster = new JFrame("Login Vokabeltrainer");
+	this.loginfenster = new JFrame("Vokabeltrainer");
 	loginfenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 	this.anmeldeButton = new JButton("Anmelden");
 	this.registrieren = new JButton("Registrieren");
 	this.exitButton = new JButton("Exit");
+	anmeldeButton.addActionListener(null);
+	registrieren.addActionListener(null);
+	exitButton.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+		
+			frmLoginSystem = new JFrame("Exit");
+			if (JOptionPane.showConfirmDialog(frmLoginSystem, "Confirm if you want to exit" ,"Vokabeltrainer",JOptionPane.YES_NO_OPTION)== JOptionPane.YES_NO_OPTION) {
+		System.exit(0); 
+		}
+	}
+	});
 	
 	this.usernameid = new JLabel("Username");
 	this.userpassword = new JLabel("Password");
+	this.frameTitle = new JLabel("Login Credentials");
 	
 	this.loginUsername = new JTextField();
-	this.loginPassword = new JTextField();
+	this.loginPassword = new JPasswordField();
 	
 	this.userpw = new JPanel();
-	this.userpwEingabe = new JPanel();
+	this.titelframe = new JPanel();
 	this.buttonBereich = new JPanel();
+	this.loginBereich = new JPanel();
 	}
 
 	public  void paint() {
 		//login_Fenster_Zeichnen
 		loginfenster.setResizable(false);
-		loginfenster.setLayout(new BorderLayout(1,45));
+		loginfenster.setLayout(new BorderLayout(10,10));
+		//
 		loginUsername.setEditable(true);
-		loginUsername.setSize(10,10);
 		loginPassword.setEditable(true);
-		loginPassword.setSize(10,10);
+		
 		 
 		//layout_für_Panels
-		userpwEingabe.setLayout(new GridLayout(2,2,-10,20));
-		userpw.setLayout(new GridLayout(2,2,-300,20));
-		userpw.setLocation(0, -300);
-		
+		titelframe.setBounds(250,1, 86, 20);
+		userpw.setLayout(new GridLayout(2,2,30,20));
+
 		//ButtonPanel
 		buttonBereich.add(anmeldeButton);
 		buttonBereich.add(registrieren);
 		buttonBereich.add(exitButton);		
-		
+		//titel
+		titelframe.add(frameTitle);
 		//Add von_loginbemerkung
 		userpw.add(usernameid);
 		userpw.add(loginUsername);
@@ -81,21 +95,36 @@ public Anmeldefenster()
 		userpw.add(loginPassword);
 		
 		//Gestaltung_loginfenster
-		loginfenster.add(userpwEingabe);
-		loginfenster.add(userpw,BorderLayout.NORTH);
-		loginfenster.add(buttonBereich,BorderLayout.CENTER);
+		loginBereich.add(userpw);
+		//
+		loginfenster.add(titelframe,BorderLayout.NORTH);
+		loginfenster.add(buttonBereich,BorderLayout.SOUTH);
+		loginfenster.add(loginBereich,BorderLayout.CENTER);
+		//
 		loginfenster.setSize(500, 200);
 		loginfenster.setVisible(true);
-		
 		//
-			     
-		
-
-}
+			         
+	}
+	class exitbtn implements ActionListener {
+	    //
+		public void actionPerformed(ActionEvent e) {
+			
+		frmLoginSystem = new JFrame("Exit");
+		if (JOptionPane.showConfirmDialog
+			(frmLoginSystem, "Confirm if you want to exit" ,"Vokabeltrainer",JOptionPane.YES_NO_OPTION)== 
+			JOptionPane.YES_NO_OPTION) {
+		System.exit(0);
+		}
+	}
+	}
+	
+	
+	
 	public static void main(String[] args) 
 	{
 		Anmeldefenster gui = new Anmeldefenster();
-		gui.paint();
-	}
-
+		gui.paint(); 
 }
+
+}		
