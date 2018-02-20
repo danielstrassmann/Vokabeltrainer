cf5b1f8080b4e93f6e8f10b9a05701d2fdf0f580
tomygun66 <t.brunner92@hotmail.com>
2018-02-20 18:10:07 +0100

    TrainingsController

--- Vokabeltrainer/src/Controller/TrainingController.java
+++ Vokabeltrainer/src/Controller/TrainingController.java
@@ -1,36 +1,57 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import Model.Daten;
+import Model.Karte;
 import Model.Kartei;
 import Model.Training;
 import Model.User;
 
 public class TrainingController {
 
+	// TODO: Attribut User ist nur ein Platzhalter. Im Konstruktor Training(u) muss
+	// ich den angemeldeten User übergeben können --> siehe Methoden trainingBox()
+	private User u;
+	private ArrayList<Karte> sammlungBox;
+
 	// Standardkonstruktor
 	public TrainingController() {
+
+	}
+
+	// Training für Box, Nummer für zu wählende Box mitgeben
+	public void boxLaden(int boxNummer) {
+		// neues Training instanzieren
+		Training t = new Training(u);
+		// Daten laden, damit diese später verändert werden können
+		Daten d = t.getTrainingsDaten();
+
+		// Kartensammlung laden
+		Kartei kk = t.getTrainingsKartei();
+		ArrayList<Karte> kartenliste;
+		kartenliste = kk.getSammlung();
+
+		
+		// Karten mit Box abfüllen
+		for (Karte k : kartenliste) {
+
+			if (k.getId() == boxNummer) {
+				sammlungBox.add(k);
+			}
+
+		}
 	}
 	
 	
-	public void starteTrainingBox1(User u) {
+	public void boxSpielen() {
 		
-		// Holt & instanziert aktive Kartei
-		Kartei kk = new Kartei();
-		ArrayList<Kartei> karteiliste;
-		karteiliste = u.getUserKarteien();
-		// provisorisch fix ID 1 --> ID muss aktive Kartei sein
-		int id = 1;
-		kk = karteiliste.get(id);
+		// Zufälliges Karte wird aus Sammlung ausgegeben
+		Random r = new Random();
+		Karte result = null;
+		result = sammlungBox.get(r.nextInt(sammlungBox.size()));
 
-		
-		// Holt & instanziert Daten vom User
-		Daten d = new Daten();
-		d = u.getUserDaten();
-		
-		// Trainingssession wird gestartet
-		Training t = new Training(kk, d);
 	}
 
 }
--- Vokabeltrainer/src/Model/Training.java
+++ Vokabeltrainer/src/Model/Training.java
@@ -1,14 +1,30 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Training {
 
 	private Kartei trainingsKartei;
 	private Daten trainingsDaten;
 
-	// Standardkonstruktor
-	public Training() {
+	// Konstruktor instanziert Training mit Userdaten
+	public Training(User u) {
+
+		// Holt & instanziert aktive Kartei
+		ArrayList<Kartei> karteiliste;
+		karteiliste = u.getUserKarteien();
+		// TODO: korrekter Listener von Klasse Hauptmenü einbinden, damit richtige
+		// Kartei geladen werden kann
+		// provisorisch fix ID 0 --> ID muss aktive Kartei sein
+		int id = 0;
+		trainingsKartei = karteiliste.get(id);
+
+		// Instanziert Daten neue Statistikdaten mit Wert 0 für neues Training
+		trainingsDaten = new Daten();
+
 	}
 
+	// Alternativer Konstruktor - für Testzwecke
 	public Training(Kartei kk, Daten d) {
 		this.setTrainingsKartei(kk);
 		this.setTrainingsDaten(d);
@@ -25,9 +41,7 @@ public class Training {
 
 	public Daten getTrainingsDaten() {
 		return trainingsDaten;
-		
-		
-		
+
 	}
 
 	public void setTrainingsDaten(Daten trainingsDaten) {
