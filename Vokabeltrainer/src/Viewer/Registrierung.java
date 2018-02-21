383265c26e905cd36f1478f7e67753aa5ca1b142
Ducij <ducij.dt@gmail.com>
2018-02-21 17:51:41 +0100

    UI mues noh noh besseret werde!

--- Vokabeltrainer/src/Viewer/Registrierung.java
+++ Vokabeltrainer/src/Viewer/Registrierung.java
@@ -17,6 +17,7 @@ public class Registrierung {
 	//
 	private JFrame registrierungsfenster;
 	//
+	private JPanel userinterface;
 	private JPanel linkeseite;
 	private JPanel rechteseite;
 	private JPanel buttons;
@@ -40,6 +41,7 @@ public class Registrierung {
 		this.linkeseite = new JPanel();
 		this.rechteseite = new JPanel();
 		this.buttons = new JPanel();
+		this.userinterface = new JPanel();
 		
 		this.btnok = new JButton("OK");
 		this.btnexit = new JButton("Exit");
@@ -50,11 +52,11 @@ public class Registrierung {
 		});
 		this.r_lblname = new JLabel("Name");
 		this.r_lblpasswort = new JLabel("Passwort");
-		this.r_lblpasswort2 = new JLabel("Passwort");
+		this.r_lblpasswort2 = new JLabel("Passwort bestätigen");
 		
-		this.r_txtname = new JTextField();
-		this.r_txtpasswort = new JPasswordField();
-		this.r_txtpasswort2 = new JPasswordField();
+		this.r_txtname = new JTextField("Name");
+		this.r_txtpasswort = new JPasswordField("Passwort");
+		this.r_txtpasswort2 = new JPasswordField("Passwort");
 		
 	}
 	public void paint() {
@@ -67,32 +69,41 @@ public class Registrierung {
 		r_txtpasswort2.setEditable(true);
 		 
 		//layout_für_Panels
+		userinterface.setLayout(new GridLayout(1,2,10,10));
 		rechteseite.setLayout(new GridLayout(3,1,10,10));
-		linkeseite.setLayout(null);
+		linkeseite.setLayout(new GridLayout(3,1,10,10));
 		//ButtonPanel
 		buttons.add(btnok);
 		buttons.add(btnexit);
 		
 		linkeseite.add(r_lblname);
-		r_lblname.setBounds(1,1,86, 20);
+		//r_lblname.setBounds(5,50,86, 20);
 		linkeseite.add(r_lblpasswort);
-		r_lblpasswort.setBounds(550,20, 86, 20);
+		//r_lblpasswort.setBounds(5,90, 86, 20);
 		linkeseite.add(r_lblpasswort2);
-		r_lblpasswort2.setBounds(550,40, 86, 20);
+		//r_lblpasswort2.setBounds(5,130, 86, 20);
 		//Add von_loginbemerkung
 		rechteseite.add(r_txtname);
-		r_txtname.setBounds(500,1, 186, 20);
+		//r_txtname.setBounds(100,1, 186, 20);
 		rechteseite.add(r_txtpasswort);
-		r_txtpasswort.setBounds(10,20, 186, 20);
+		//r_txtpasswort.setBounds(10,20, 186, 20);
 		rechteseite.add(r_txtpasswort2);
-		r_txtpasswort2.setBounds(50,60, 186, 20);
+		
+		//r_txtpasswort2.setBounds(20,20, 1, 2);
 		//Gestaltung_loginfenster
-		
-		
-		registrierungsfenster.add(linkeseite);
-		registrierungsfenster.add(rechteseite);
+		userinterface.add(linkeseite);
+		userinterface.add(rechteseite);
+		//registrierungsfenster.add(r_lblname);
+		//registrierungsfenster.add(r_lblpasswort);
+		//registrierungsfenster.add(r_lblpasswort2);
+		//registrierungsfenster.add(r_txtname);
+		//registrierungsfenster.add(r_txtpasswort);
+		//registrierungsfenster.add(r_txtpasswort2);
+		//registrierungsfenster.add(linkeseite, BorderLayout.WEST);
+		//registrierungsfenster.add(rechteseite, BorderLayout.CENTER);
 		registrierungsfenster.add(buttons,BorderLayout.SOUTH);
-		//
+		registrierungsfenster.add(userinterface, BorderLayout.CENTER);
+		//r_txtname.setSize(10,5);
 		registrierungsfenster.setSize(1000, 500);
 		registrierungsfenster.setVisible(true);
 		//
