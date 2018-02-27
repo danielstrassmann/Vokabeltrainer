0e1ccdf691c4bb57c7b229b0b450e6ebafbb73de
Atrais <marius.braendle@gmx.ch>
2018-02-27 14:06:51 +0100

    TabLernen

--- Vokabeltrainer/src/Viewer/Tab/TabLernen.java
+++ Vokabeltrainer/src/Viewer/Tab/TabLernen.java
@@ -3,6 +3,7 @@ package Viewer.Tab;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -25,6 +26,12 @@ public class TabLernen extends JPanel implements ActionListener
 	private JLabel boxLabel4;
 	private JLabel boxLabel5;
 	
+	private JLabel box1;
+	private JLabel box2;
+	private JLabel box3;
+	private JLabel box4;
+	private JLabel box5;
+	
 	private MenuleisteOben mlo;
 	
 	public TabLernen(MenuleisteOben mlo)
@@ -37,11 +44,18 @@ public class TabLernen extends JPanel implements ActionListener
 	
 	private void tabLernen()
 	{
-		this.boxLabel1 = new JLabel("Box 1");
-		this.boxLabel2 = new JLabel("Box 2");
-		this.boxLabel3 = new JLabel("Box 3");
-		this.boxLabel4 = new JLabel("Box 4");
-		this.boxLabel5 = new JLabel("Box 5");
+		this.boxLabel1 = new JLabel();
+		this.boxLabel2 = new JLabel();
+		this.boxLabel3 = new JLabel();
+		this.boxLabel4 = new JLabel();
+		this.boxLabel5 = new JLabel();
+		
+		this.box1 = new JLabel("BOX 1");
+		this.box2 = new JLabel("BOX 2");
+		this.box3 = new JLabel("BOX 3");
+		this.box4 = new JLabel("BOX 4");
+		this.box5 = new JLabel("BOX 5");
+
 		
 		boxLabel1.addMouseListener(new boxMausKlick());
 		boxLabel2.addMouseListener(new boxMausKlick());	
@@ -68,12 +82,41 @@ public class TabLernen extends JPanel implements ActionListener
 		boxLabel4.setPreferredSize(new Dimension(200,700));
 		boxLabel5.setPreferredSize(new Dimension(200,700));
 		
-		boxLabel1.setBackground(Color.BLUE);
-		boxLabel2.setBackground(Color.RED);
-		boxLabel3.setBackground(Color.BLUE);
-		boxLabel4.setBackground(Color.BLUE);
-		boxLabel5.setBackground(Color.BLUE);
-					
+		boxLabel1.setLocation(10, 20);
+
+
+		boxLabel1.setBackground(new Color(100, 120,100));
+		boxLabel1.setOpaque(true);
+		boxLabel2.setBackground(new Color(100, 140,100));
+		boxLabel2.setOpaque(true);
+		boxLabel3.setBackground(new Color(100, 160,100));
+		boxLabel3.setOpaque(true);
+		boxLabel4.setBackground(new Color(100, 180,100));
+		boxLabel4.setOpaque(true);
+		boxLabel5.setBackground(new Color(100, 200, 100));
+		boxLabel5.setOpaque(true);
+		
+
+		
+		box1.setBounds(50, 350, 300, 25);
+		box2.setBounds(50, 350, 300, 25);
+		box3.setBounds(50, 350, 300, 25);
+		box4.setBounds(50, 350, 300, 25);
+		box5.setBounds(50, 350, 300, 25);
+		
+		box1.setFont(new Font( "Bradley Hand ITC", Font.BOLD, 34));
+		box2.setFont(new Font( "Bradley Hand ITC", Font.BOLD, 34));
+		box3.setFont(new Font( "Bradley Hand ITC", Font.BOLD, 34));
+		box4.setFont(new Font( "Bradley Hand ITC", Font.BOLD, 34));
+		box5.setFont(new Font( "Bradley Hand ITC", Font.BOLD, 34));
+
+		
+		boxLabel1.add(box1);
+		boxLabel2.add(box2);
+		boxLabel3.add(box3);
+		boxLabel4.add(box4);
+		boxLabel5.add(box5);
+		
 		add(boxLabel1);
 		add(boxLabel2);
 		add(boxLabel3);
