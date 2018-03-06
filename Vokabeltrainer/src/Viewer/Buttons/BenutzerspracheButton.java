31a3263ce5f020ed900ce990e5160a7404423be1
Ducij <ducij.dt@gmail.com>
2018-03-06 10:48:37 +0100

    Sprache	Statistik

--- Vokabeltrainer/src/Viewer/Buttons/BenutzerspracheButton.java
+++ Vokabeltrainer/src/Viewer/Buttons/BenutzerspracheButton.java
@@ -2,15 +2,28 @@ package Viewer.Buttons;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.util.Locale;
 
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
+import Model.User;
+
+
+
 public class BenutzerspracheButton extends JComboBox
+
 {
+
+	User u;
+	String value = new String();
+
+
 		public BenutzerspracheButton()
 		{
 			benutzerSprache();
+			
+			
 		}
 		
 		public void benutzerSprache()
@@ -22,6 +35,35 @@ public class BenutzerspracheButton extends JComboBox
 
 			setVisible(true);
 			setSize(200,200);
+		}
+		
+		public void spracheAendern(User u) 
+		
+		{
+			u.getBenutzersprache();
+			
+				this.bsp = new BenutzerspracheButton();
+				if (String value = (String) benutzerSprache.getSelectedItem().equals("Deutsch"));{
+					//Locale l = new Locale(u.getBenutzersprache());
+					//ResourceBundle r = ResourceBundle.getBundle("Bundle_Deutsch", l);
+				}
+				else if (String value = (String) benutzerSprache.getSelectedItem().equals("francais")); {
+					//Locale l = new Locale(u.getBenutzersprache());
+					//ResourceBundle r = ResourceBundle.getBundle("Bundle_francais", l);
+				}
+				else if (String value = (String) benutzerSprache.getSelectedItem().equals("english")); {
+					//Locale l = new Locale(u.getBenutzersprache());
+					//ResourceBundle r = ResourceBundle.getBundle("Bundle_english", l);
+				}
+				else if (String value = (String) benutzerSprache.getSelectedItem().equals("italiano")); {
+					//Locale l = new Locale(u.getBenutzersprache());
+					//ResourceBundle r = ResourceBundle.getBundle("Bundle_italiano", l);
+				}
+				else return
+						
+						
+			
+			
 		}
 
 }
